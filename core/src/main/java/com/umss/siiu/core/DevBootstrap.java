311639493cf93f3bd63be28b03db00fc6149229c
tercere <tercere@labcorp.com>
2021-11-17 18:51:43 -0400

    mas de DevBootstrap

--- core/src/main/java/com/umss/siiu/core/DevBootstrap.java
+++ core/src/main/java/com/umss/siiu/core/DevBootstrap.java
@@ -4,11 +4,14 @@
 
 package com.umss.siiu.core;
 
+import com.umss.siiu.core.model.Buy;
 import com.umss.siiu.core.model.Employee;
 import com.umss.siiu.core.repository.*;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
@@ -18,21 +21,31 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private EmployeeRepository employeeRepository;
     private PositionRepository positionRepository;
     private ContractRepository contractRepository;
+    private BuyRepository buyRepository;
 
     public DevBootstrap(CategoryRepository categoryRepository, SubCategoryRepository subCategoryRepository,
             ItemRepository itemRepository, EmployeeRepository employeeRepository,
-            PositionRepository positionRepository, ContractRepository contractRepository) {
+            PositionRepository positionRepository, ContractRepository contractRepository, BuyRepository buyRepository) {
         this.categoryRepository = categoryRepository;
         this.subCategoryRepository = subCategoryRepository;
         this.itemRepository = itemRepository;
         this.employeeRepository = employeeRepository;
         this.positionRepository = positionRepository;
         this.contractRepository = contractRepository;
+        this.buyRepository = buyRepository;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         createEmployees();
+        persistBuy(BigDecimal.TEN);
+        persistBuy(BigDecimal.ONE);
+    }
+
+    private void persistBuy(BigDecimal value) {
+        Buy buy = new Buy();
+        buy.setValue(value);
+        buyRepository.save(buy);
     }
 
     private void createEmployees() {
